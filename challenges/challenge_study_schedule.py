135b3e75450184f7bbbbc942c5ee5204ba63c8e7
heidegomes <heidegc@hotmail.com>
2023-08-18 15:33:19 -0300

    Iniciando projeto Algoritmos!

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    saida = 0
+    n = len(permanence_period)  # N será a quantidade de elementos da lista
+    for index in range(0, n):  # vamos iterar a lista completa
+        if permanence_period[index] == target_time:
+            saida += 1
+
+    return saida
     raise NotImplementedError
+
+
+print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 2))
