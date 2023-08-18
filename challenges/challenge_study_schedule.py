f0a4f7ea41850cbf8163d05474a7e2ea7a5d421e
heidegomes <heidegc@hotmail.com>
2023-08-18 17:03:37 -0300

    Requisito 1 feito!

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,14 @@
 def study_schedule(permanence_period, target_time):
     saida = 0
-    n = len(permanence_period)
-    for index in range(0, n):
-        if permanence_period[index] == target_time:
+    if not target_time:
+        return None
+    for i in permanence_period:
+        if not isinstance(i[0], int) or not isinstance(i[1], int):
+            return None
+        if i[0] <= target_time <= i[1]:
             saida += 1
 
     return saida
-    raise NotImplementedError
 
 
 print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 2))
