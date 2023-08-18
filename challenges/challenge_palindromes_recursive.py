686093efa3acbd8889caa112d0f21556fc2c8732
heidegomes <heidegc@hotmail.com>
2023-08-18 19:55:16 -0300

    Requisito 3 feito!

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    if low_index >= high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+# print(is_palindrome_recursive(0, len("brara") - 1))
