d969f36eda42666cc918e3fcc39968f6a37317e5
heidegomes <heidegc@hotmail.com>
2023-08-21 19:52:40 -0300

    Requisito 2 feito!

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,10 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message("string", "1")
+    assert encrypt_message("string", 6) == "gnirts"
+    assert encrypt_message("string", -2) == "gnirts"
+    assert encrypt_message("string", 2) == "gnir_ts"
