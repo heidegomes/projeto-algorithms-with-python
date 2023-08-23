844b5db7e93afd6032849853c7fed3bbfd4cf85f
heidegomes <heidegc@hotmail.com>
2023-08-23 14:50:10 -0300

    Requisito 6 feito!

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+    low = 0
+    high = len(word) - 1
+    while low < high:
+        if word[low] != word[high]:
+            return False
+        low += 1
+        high -= 1
+    return True
+
+
+print(is_palindrome_iterative("racecar"))
