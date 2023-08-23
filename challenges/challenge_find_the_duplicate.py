7d55c4f10059005db059fa142e33042550a251b6
heidegomes <heidegc@hotmail.com>
2023-08-23 19:12:41 -0300

    Requisito 5 feito!

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,16 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+    for i in range(1, len(nums)):
+        if isinstance(nums[i], str) or nums[i] < 0:
+            return False
+        if nums[i] == nums[i - 1]:
+            return nums[i]
+    return False
+
+    # return len(nums) != len(set(nums))
+    # nums.sort()
+    # duplicate = {}
+    # for num in nums:
+    #     if num in duplicate:
+    #         return num
+    #     duplicate[num] = 1
